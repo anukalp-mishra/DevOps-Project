32fafa1ce1c77541b6de534ac592b8ff1f821dce
Anukalp Mishra <58182766+anukalp-mishra@users.noreply.github.com>
2025-10-05 00:47:41 +0530

    Update main.tf

--- Terraform/Module/ec2_instance/main.tf
+++ Terraform/Module/ec2_instance/main.tf
@@ -3,6 +3,17 @@ provider "aws" {
 }
 
 resource "aws_instance" "new_instance" {
-    ami = ""
-    instance_type = ""
+    ami = "ami-02d26659fd82cf299"
+    instance_type = var.instance_type
+    tags = {
+        Name = "Terraform EC2"
+    }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+    bucket = "my-s3-bucket-terraform-21"
+}
+
+output "my_output" {
+    value = aws_instance.new_instance.public_ip
 }
