80f2132b17b47096765c3ffebe66f2110b0cefc3
Anukalp Mishra <anukalp.info@gmail.com>
2025-10-05 23:58:56 +0530

    Terraform.tfstate file moved to S3 bucket

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,10 +12,10 @@ module "iam_user" {
   user_names = var.user_names
 }
 
-/*terraform {
+terraform {
     backend "s3" {
         bucket = "my-s3-bucket-terraform-21"
         key = "key/terraform.tfstate"
         region = "ap-south-1"
     }
-}*/
+}
