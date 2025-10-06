698b255711d834aba799e81c373ad2e2faaa3d31
Anukalp Mishra <anukalp.info@gmail.com>
2025-10-07 03:20:35 +0530

    Added code to install Apache server

--- Terraform/modules/ec2_instance/main.tf
+++ Terraform/modules/ec2_instance/main.tf
@@ -24,6 +24,17 @@ resource "aws_instance" "new_instance" {
     command = "touch hello_terra.tf"
   }
 
+
+    user_data = <<-EOF
+      #!/bin/sh
+      sudo apt-get update
+      sudo apt install -y apache2
+      sudo systemctl status apache2
+      sudo systemctl start apache2
+      sudo chown -R $USER:$USER /var/www/html
+      sudo echo "<html><body><h1>Hello from Terraform instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
+      EOF
+
     tags = {
         Name = "Terraform EC2"
     }
